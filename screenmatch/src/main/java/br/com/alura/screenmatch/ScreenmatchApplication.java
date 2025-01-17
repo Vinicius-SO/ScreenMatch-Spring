d55db291090cc31ed340347effd932ff4dbe98c7
unknown <vhisoares39@gmail.com>
2025-01-17 10:26:28 -0300

    Add:Modeling api data

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.services.ConsumoAPI;
+import br.com.alura.screenmatch.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +17,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoAPI();
-		var response = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=cf857f84");
-		System.out.println(response);
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=cf857f84");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie serie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(serie);
 	}
 }
